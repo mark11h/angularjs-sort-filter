a53761828e2825a680d77d40090f15d3d66eea20
Mark Hinchliffe <mark11h@live.co.uk>
2017-11-14 20:42:18 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = angular.module('App', []);
+app.controller('AppController', function($scope) {
+	$scope.contacts = [
+		{
+			name: 'Mark',
+			age: 34,
+			gender: 'Male'
+		},
+		{
+			name: 'Sarah',
+			age: 29,
+			gender: 'Female'
+		},
+		{
+			name: 'Jacob',
+			age: 5,
+			gender: 'Male'
+		},
+		{
+			name: 'Abigail',
+			age: 0,
+			gender: 'Female'
+		}];	
+});
