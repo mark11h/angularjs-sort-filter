373173c8480919babd24f5628efa70350e04c8ec
Mark Hinchliffe <mark11h@live.co.uk>
2017-11-22 20:35:47 +0000

    Added first filter for gender

--- app.js
+++ app.js
@@ -23,4 +23,67 @@ app.controller('AppController', function($scope) {
 		}];
 	$scope.sortOrderOptions = ['name','age','gender'];
 	$scope.sortOrder = 'name';
+	$scope.genderFilter = [];
+	
+	for (var i = 0; i < $scope.contacts.length; i++) {
+		buildGenderFilter($scope.contacts[i]);
+	}
+	
+	function buildGenderFilter(contact) {
+		var gender = {
+			name: contact.gender,
+			count: 1,
+			selected: false
+		};
+    
+		var genderAdded = false;
+		for (var i = 0; i < $scope.genderFilter.length; i++) {
+			if ($scope.genderFilter[i].name === contact.gender) {
+				$scope.genderFilter[i].count++;
+				genderAdded = true;
+				break;
+			}
+		}
+    
+		if (!genderAdded) {
+			$scope.genderFilter.push(gender);
+		}
+	}
+})
+.filter('byGender', function () {
+	return function (items, gender) {
+
+		if (typeof items == 'undefined') {
+			return;
+		}
+
+		if (typeof gender == 'undefined') {
+			return items;
+		}
+
+		var noneSelected = true;
+		for (var j = 0; j < gender.length; j++) {
+			if (gender[j].selected) {
+				noneSelected = false;
+			}
+		}
+
+		if (noneSelected) {
+			return items;
+		}
+
+		var filtered = [];
+
+		for (var i = 0; i < items.length; i++) {
+			var item = items[i];
+
+			for (var j = 0; j < gender.length; j++) {
+				if (item.gender == gender[j].name && gender[j].selected) {
+					filtered.push(item);
+				}
+			}			
+		}
+
+		return filtered;
+	}
 });
